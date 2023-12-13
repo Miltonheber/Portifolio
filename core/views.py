cffd17acc9e7be84182490761d7b862c85f252a9
Miltonheber <miltonheber1@gmail.com>
2023-12-13 22:40:54 +0200

    FEAT| adding urls and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
 
+def home(request):
+    contexto = {}
+    return render(request, 'home.html', contexto)
 
+def projectos(request):
+    
+    contexto = {}
+    return render(request, 'projectos.html', contexto)
