6f5b955c213bc908f4362da0c92aa646ccb4da46
Miltonheber <miltonheber1@gmail.com>
2023-12-13 22:07:20 +0200

    FEAT| adding new models: Servico and Habilidade

--- core/models.py
+++ core/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+class Servico(models.Model):
+    
+    imagem = models.ImageField(upload_to='icones-servicos')
+    servico = models.CharField('serviço', null=False, max_length=100)
+    descricao = models.TextField('descriçao', null=False)
+    
+    def __str__(self):
+        return self.servico
+
+class Habilidade(models.Model):
+    ferramenta = models.CharField('ferramenta', null=False, unique=True, max_length=100 )
+    percentagem = models.IntegerField(null=False, max_length=3)
+    
+    def __str__(self):
+        return self.ferramenta
+    
+    
 class Projecto(models.Model):
     
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
